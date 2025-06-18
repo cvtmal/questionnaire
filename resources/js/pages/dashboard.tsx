fc6634cffa5f4abcd1cadcab7fac147c35997b2d
--replace-all <--replace-all>
2025-06-18 11:28:13 +0200

    fix: dashboard

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -42,7 +42,7 @@ export default function Dashboard() {
                     </Card>
                     <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">Active Interviews</CardTitle>
+                            <CardTitle className="text-sm font-medium">Interviews with companies</CardTitle>
                             <BarChart3Icon className="text-muted-foreground h-4 w-4" />
                         </CardHeader>
                         <CardContent>
